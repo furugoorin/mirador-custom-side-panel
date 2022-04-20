bf5dcc760ed22ba3530c7dda173796fbd75bfe99
Crystal Chu <cchu618@gmail.com>
2022-04-20 12:14:39 -0400

    updated import in demo

--- custom-side-panel/demo/src/index.js
+++ custom-side-panel/demo/src/index.js
@@ -1,5 +1,5 @@
 import Mirador from 'mirador';
-import CustomSidePanel from '../../src/components/CustomSidePanel'
+import CustomSidePanel from '../../src/components/index'
 
 const config = { 
   id: 'demo', 
