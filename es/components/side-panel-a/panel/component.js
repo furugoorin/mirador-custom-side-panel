d2345472439885666d303268f7a20594dd13bb81
Crystal Chu <cchu618@gmail.com>
2022-05-02 13:15:48 -0400

    updated export

--- es/components/side-panel-a/panel/component.js
+++ es/components/side-panel-a/panel/component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CompanionWindow from 'mirador/dist/es/src/containers/CompanionWindow';
+import PropTypes from 'prop-types';
 export default function CustomSidePanel(props) {
   var id = props.id,
       windowId = props.windowId,
@@ -9,4 +10,12 @@ export default function CustomSidePanel(props) {
     windowId: windowId,
     id: id
   }, /*#__PURE__*/React.createElement("h1", null, "This is a work in progress"), /*#__PURE__*/React.createElement("p", null, "\xB0\u02D6\u2727\u25DD(\u2070\u25BF\u2070)\u25DC\u2727\u02D6\xB0"), /*#__PURE__*/React.createElement("p", null, "What does this panel look like with more text?"), /*#__PURE__*/React.createElement("p", null, "It seemed like it should have been so simple. There was nothing inherently difficult with getting the project done. It was simple and straightforward enough that even a child should have been able to complete it on time, but that wasn't the case. The deadline had arrived and the project remained unfinished."));
-}
+} // Declares prop to be a specific type:
+
+CustomSidePanel.propTypes = process.env.NODE_ENV !== "production" ? {
+  title: PropTypes.string
+} : {}; // Specifies the default values for props:
+
+CustomSidePanel.defaultProps = {
+  title: 'Unnamed Panel Title'
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,6 @@
 import CustomPanelA from './side-panel-a/panel/plugin';
 import CustomButtonA from './side-panel-a/button/plugin';
 
-export {
-  CustomPanelA,
-  CustomButtonA,
-};
-
 export default [
   CustomPanelA,
   CustomButtonA,
